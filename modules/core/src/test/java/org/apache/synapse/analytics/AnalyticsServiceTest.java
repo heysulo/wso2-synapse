dc48bb35528f483faf578df234b8a577ca4b96f7
Sulochana Kodituwakku <sulochana@wso2.com>
2022-07-13 23:37:37 +0530

    Implement custom analytics unit test

--- modules/core/src/test/java/org/apache/synapse/analytics/AnalyticsServiceTest.java
+++ modules/core/src/test/java/org/apache/synapse/analytics/AnalyticsServiceTest.java
@@ -37,12 +37,14 @@ import org.apache.synapse.api.Resource;
 import org.apache.synapse.api.rest.RestRequestHandler;
 import org.apache.synapse.config.SynapseConfiguration;
 import org.apache.synapse.config.xml.SequenceMediatorFactory;
+import org.apache.synapse.config.xml.XMLConfigConstants;
 import org.apache.synapse.config.xml.endpoints.HTTPEndpointFactory;
 import org.apache.synapse.core.axis2.*;
 import org.apache.synapse.endpoints.EndpointDefinition;
 import org.apache.synapse.endpoints.HTTPEndpoint;
 import org.apache.synapse.mediators.TestUtils;
 import org.apache.synapse.mediators.base.SequenceMediator;
+import org.apache.synapse.mediators.builtin.PropertyMediator;
 import org.apache.synapse.transport.nhttp.NhttpConstants;
 import org.jetbrains.annotations.NotNull;
 import org.mockito.Mockito;
@@ -65,6 +67,7 @@ public class AnalyticsServiceTest extends TestCase {
     private static final String TEST_API_URL = "/test/admin?search=wso2";
     private static final String TEST_API_METHOD = "GET";
     private static final String TEST_API_PROTOCOL = "https";
+    private static final String TEST_SEQUENCE_NAME = "TestSequenceName";
 
     private static final int CURRENT_SCHEMA_VERSION = 1;
     private final SimpleAnalyticsService service = new SimpleAnalyticsService();
@@ -155,7 +158,7 @@ public class AnalyticsServiceTest extends TestCase {
 
     public void testSequenceAnalytics() {
         SequenceMediator seq = new SequenceMediator();
-        seq.setName("SequenceName");
+        seq.setName(TEST_SEQUENCE_NAME);
 
         seq.mediate(messageContext);
         assertEquals(1, service.getAvailableAnalyticsCount());
@@ -191,12 +194,9 @@ public class AnalyticsServiceTest extends TestCase {
     }
 
     public void testProxyServiceAnalytics() throws XMLStreamException, AxisFault {
-        //create ProxyServiceMessageReceiver instance
         ProxyServiceMessageReceiver proxyServiceMessageReceiver = new ProxyServiceMessageReceiver();
         ProxyService proxyService = new ProxyService("TestProxy");
-        //create an inSequence and set
         OMElement sequenceAsOM = AXIOMUtil.stringToOM("<inSequence xmlns=\"http://ws.apache.org/ns/synapse\">\n"
-                + "         <property name=\"TEST\" scope=\"axis2\" type=\"STRING\" value=\"WSO2\"/>\n"
                 + "      </inSequence>");
         proxyService.setTargetInLineInSequence(new SequenceMediatorFactory().
                 createAnonymousSequence(sequenceAsOM, new Properties()));
@@ -207,11 +207,94 @@ public class AnalyticsServiceTest extends TestCase {
         MessageContextCreatorForAxis2.setSynEnv(synapseEnvironment);
         messageContext.setEnvironment(synapseEnvironment);
 
-        //invoke
         proxyServiceMessageReceiver.receive(messageContext.getAxis2MessageContext());
         assertEquals(2, service.getAvailableAnalyticsCount());
         verifySchema(service.fetchAnalytic(), AnalyticPayloadType.SEQUENCE);
         verifySchema(service.fetchAnalytic(), AnalyticPayloadType.PROXY_SERVICE);
+    }
+
+    public void testAnalyticsMetadata() {
+        SequenceMediator seq = new SequenceMediator();
+        seq.setName(TEST_SEQUENCE_NAME);
+
+        PropertyMediator analyticBoolean = new PropertyMediator();
+        analyticBoolean.setName("analyticBoolean");
+        analyticBoolean.setAction(PropertyMediator.ACTION_SET);
+        analyticBoolean.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticBoolean.setValue(String.valueOf(true));
+
+        PropertyMediator analyticDouble = new PropertyMediator();
+        analyticDouble.setName("analyticDouble");
+        analyticDouble.setAction(PropertyMediator.ACTION_SET);
+        analyticDouble.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticDouble.setValue(String.valueOf(Double.MAX_VALUE));
+
+        PropertyMediator analyticFloat = new PropertyMediator();
+        analyticFloat.setName("analyticFloat");
+        analyticFloat.setAction(PropertyMediator.ACTION_SET);
+        analyticFloat.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticFloat.setValue(String.valueOf(Float.MAX_VALUE));
+
+        PropertyMediator analyticInteger = new PropertyMediator();
+        analyticInteger.setName("analyticInteger");
+        analyticInteger.setAction(PropertyMediator.ACTION_SET);
+        analyticInteger.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticInteger.setValue(String.valueOf(Integer.MAX_VALUE));
+
+        PropertyMediator analyticLong = new PropertyMediator();
+        analyticLong.setName("analyticLong");
+        analyticLong.setAction(PropertyMediator.ACTION_SET);
+        analyticLong.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticLong.setValue(String.valueOf(Long.MAX_VALUE));
+
+        PropertyMediator analyticShort = new PropertyMediator();
+        analyticShort.setName("analyticShort");
+        analyticShort.setAction(PropertyMediator.ACTION_SET);
+        analyticShort.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticShort.setValue(String.valueOf(Short.MAX_VALUE));
+
+        PropertyMediator analyticString = new PropertyMediator();
+        analyticString.setName("analyticString");
+        analyticString.setAction(PropertyMediator.ACTION_SET);
+        analyticString.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticString.setValue("WSO2");
+
+        seq.addChild(analyticBoolean);
+        seq.addChild(analyticDouble);
+        seq.addChild(analyticFloat);
+        seq.addChild(analyticInteger);
+        seq.addChild(analyticLong);
+        seq.addChild(analyticShort);
+        seq.addChild(analyticString);
+
+        seq.mediate(messageContext);
+        assertEquals(1, service.getAvailableAnalyticsCount());
+        JsonObject publishedAnalytic = service.fetchAnalytic();
+        verifySchema(publishedAnalytic, AnalyticPayloadType.SEQUENCE);
+
+        JsonObject metadata = publishedAnalytic.getAsJsonObject(AnalyticsConstants.EnvelopDef.PAYLOAD)
+                .getAsJsonObject(AnalyticsConstants.EnvelopDef.METADATA);
+
+        assertTrue(metadata.has("analyticBoolean"));
+        assertTrue(metadata.get("analyticBoolean").getAsBoolean());
+
+        assertTrue(metadata.has("analyticDouble"));
+        assertEquals(Double.MAX_VALUE, metadata.get("analyticDouble").getAsDouble());
+
+        assertTrue(metadata.has("analyticFloat"));
+        assertEquals(Float.MAX_VALUE, metadata.get("analyticFloat").getAsFloat());
+
+        assertTrue(metadata.has("analyticInteger"));
+        assertEquals(Integer.MAX_VALUE, metadata.get("analyticInteger").getAsInt());
+
+        assertTrue(metadata.has("analyticLong"));
+        assertEquals(Long.MAX_VALUE, metadata.get("analyticLong").getAsLong());
+
+        assertTrue(metadata.has("analyticShort"));
+        assertEquals(Short.MAX_VALUE, metadata.get("analyticShort").getAsShort());
+
+        assertTrue(metadata.has("analyticString"));
+        assertEquals("WSO2", metadata.get("analyticString").getAsString());
     }
 
     private void verifySchema(JsonObject analytic, @NotNull AnalyticPayloadType payloadType) {
